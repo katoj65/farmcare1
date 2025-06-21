551b76ae949e7f922bcbcbcfdf89857f7574308e
katoj65 <katoj652gmail.com>
2025-06-21 15:18:06 +0300

    Added farm page

--- src/views/Farm.vue
+++ src/views/Farm.vue
@@ -1,18 +1,35 @@
 <script setup lang="ts">
 import AppLayout from '@/components/AppLayout.vue';
 import {db} from '@/Database/database';
-
+import { IonItem, IonLabel,  IonFab, IonFabButton, IonIcon } from '@ionic/vue';
+import { caretForwardOutline, add } from 'ionicons/icons';
 
 
 </script>
 <template>
 <app-layout title="Farm">
 
+<ion-item detail="true" v-for="(e,key) in 30" :key="key">
+<ion-label>
+<h3>Name of farm</h3>
+<p>
+Kampala - Uganda
+</p>
+</ion-label>
+</ion-item>
 
 
 
 
 
 
+
+
+
+<ion-fab slot="fixed" vertical="bottom" horizontal="end">
+    <ion-fab-button>
+      <ion-icon :icon="add"></ion-icon>
+    </ion-fab-button>
+  </ion-fab>
 </app-layout>
 </template>
