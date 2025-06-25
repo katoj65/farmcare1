ac18a99cf3ce1d8ca42042685db016f7284f8ea1
katoj65 <katoj652gmail.com>
2025-06-25 16:46:51 +0300

    Updates

---
+++ assets/status-tap-BYjZCUkt.js
@@ -0,0 +1,3 @@
+import{ar as r,as as i,at as c,au as d,av as l}from"./index-pbNnSScs.js";/*!
+ * (C) Ionic http://ionicframework.com - MIT License
+ */const m=()=>{const e=window;e.addEventListener("statusTap",()=>{r(()=>{const o=e.innerWidth,s=e.innerHeight,n=document.elementFromPoint(o/2,s/2);if(!n)return;const t=i(n);t&&new Promise(a=>c(t,a)).then(()=>{d(async()=>{t.style.setProperty("--overflow","hidden"),await l(t,300),t.style.removeProperty("--overflow")})})})})};export{m as startStatusTap};
